68f75d34e53d9ace1ce507a9360f4fb3704afd43
Blair <blair.butterworth@gmail.com>
2017-12-07 12:57:43 +0000

    Added comments

--- src/main/java/org/ucl/msr/utils/stream/StreamUtils.java
+++ src/main/java/org/ucl/msr/utils/stream/StreamUtils.java
@@ -1,3 +1,12 @@
+/*
+ * UCL MSR Processor (c) by Blair Butterworth and Chenghui Fan
+ *
+ * This work is licensed under the Creative Commons Attribution 4.0
+ * International License. To view a copy of this license, visit
+ *
+ *      http://creativecommons.org/licenses/by/4.0/
+ */
+
 package org.ucl.msr.utils.stream;
 
 import org.apache.commons.io.IOUtils;
@@ -7,8 +16,19 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
+/**
+ * Instances of this class provide utility functions for working with
+ * {@link InputStream}s and {@link java.io.OutputStream}s
+ *
+ * @author Blair Butterworth
+ * @author Chenghui Fan
+ */
 public class StreamUtils
 {
+    private StreamUtils()
+    {
+    }
+
     public static String getString(InputStream inputStream) throws IOException
     {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
